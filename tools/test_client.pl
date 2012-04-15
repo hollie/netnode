0e0b7782d99fc9d4f3f499af314387e269fc08ad
Lieven Hollevoet <hollie@lika.be>
2012-04-15 21:42:18 +0200

    Oops, TCP packet should end with \r\n\r\n

--- tools/test_client.pl
+++ tools/test_client.pl
@@ -10,10 +10,10 @@ use IO::Socket;
 use Net::hostent;  
 use Data::Dumper;
 
-#my $server_port = 80;
-#my $host = "api.pachube.com";
-my $server_port = 8080;
-my $host = "192.168.1.32";
+my $server_port = 80;
+my $host = "api.pachube.com";
+#my $server_port = 8080;
+#my $host = "192.168.1.32";
 
 my $remote = IO::Socket::INET->new( Proto     => 'tcp',
 				     PeerAddr => $host,
@@ -24,8 +24,8 @@ my $remote = IO::Socket::INET->new( Proto     => 'tcp',
 unless ($remote) {die "cannot connect to server"}
 print "Connected to $host\n";
 
-$remote->autoflush(1);
-print $remote json(71) . "}";
+#$remote->autoflush(1);
+print $remote json(65);
 
 while (<$remote>) {
 	my $line = $_;
@@ -40,7 +40,8 @@ sub json {
 	my $humi = shift();
 
 	return <<END;
-PUT / HTTP/1.1\r\nUser-Agent: curl/0.1\r
+PUT /v2/feeds/55686 HTTP/1.1\r
+User-Agent: hollie/script\r
 Host: $host:$server_port\r
 Accept: */*\r
 X-PachubeApiKey: OivrtCBI0vaCBGTN46ktyluuoqeSAKxzZXlZUEdzdGlRYz0g\r
@@ -51,6 +52,8 @@ Content-Type: application/x-www-form-urlencoded\r
   \"version\":\"1.0.0\",
   \"datastreams\":[
     {\"id\":\"humidity\", \"current_value\":\"$humi\"}
-  ]
+      ]\n
+}\r
+\r
 END
 }
